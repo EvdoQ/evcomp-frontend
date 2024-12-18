b0f6a78ff4cfad43f35baf4e410502c8f5f6868f
E. Borodov <mgopjik@gmail.com>
2024-12-18 21:11:19 +0300

    feat: add footer and header

---
+++ src/Components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="Footer fixed-bottom text-center p-3 bg-dark text-white">
+            <i className="bi bi-cup-straw"></i> Made by EvdoQ
+        </div>
+    );
+}
+
+export default Footer;
--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Footer, Header } from "../Components/Layout";
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <header className="App-header">Evcomp</header>
+            <Footer />
         </div>
     );
 }
